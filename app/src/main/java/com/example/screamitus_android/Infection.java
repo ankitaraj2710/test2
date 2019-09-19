c75b0de6335e9f7d3f89567e8e34326c0b29f85d
macstudent <ankitrajkaur@gmail.com>
2019-09-19 11:54:46 -0400

    first

---
+++ app/src/main/java/com/example/screamitus_android/Infection.java
@@ -0,0 +1,51 @@
+package com.example.screamitus_android;
+
+public class Infection {
+
+   public int calculateTotalInfected(int day) {
+       int total = 50;
+       int calculateTotalInfected = total/5;
+       //R1 the number of days must be > 0
+       if (n <= 0) {
+           System.out.println("-1");
+       } else {
+           System.out.println("n");
+       }
+//R2 the virus infects intructor by rate of 5
+       public class infectedPerson{
+           int infectedPerson=n;
+           int days = m;
+           int rate = total/5;
+           System.out.println("rate is": + rate );
+           //R3 the rate increases to 8 instructor
+           int rate = total/8;
+       }
+
+   }
+
+    public class BMICalculator {
+
+        public String calculateBMI(double weight, double height) {
+            double bmi = (weight) / (height * height);
+            System.out.println("BMI: " + bmi);
+
+            if (bmi > 18.5 && bmi < 25 ) {
+                return "normal";
+            }
+            else if (bmi >= 25 && bmi < 30) {
+                return "average";
+            }
+            else if (bmi >= 31 && bmi < 40) {
+                return "fat";
+            }
+            else if (bmi >= 40) {
+                return "severe fat";
+            }
+            else {
+                return "aliens!";
+            }
+
+        }
+    }
+
+}
---
+++ app/src/test/java/com/example/screamitus_android/ExampleUnitTest.java
@@ -0,0 +1,20 @@
+package com.example.screamitus_android;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+
+
+    @Test
+    public void infection() {
+        int n = 2;
+        assertEquals(2,n);
+    }
+}
